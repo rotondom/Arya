4eb7425674275eaa5df4e35240f83a1d5fe84ae2
Bryan Young <youngb2@wit.edu>
2012-11-07 17:32:39 -0500

    Added data folder for game resources

---
+++ arya/src/arya/BlockMap.java
@@ -0,0 +1,12 @@
+package arya;
+
+import java.util.ArrayList;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.tiled.TiledMap;
+
+public class BlockMap {
+	public static TiledMap tmap;
+	public static int mapWidth;
+	public static int mapHeight;
+	
+}
