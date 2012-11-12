5b1a2e1664a5c25aaa76a4b77587fdeb20461c2c
Bryan Young <youngb2@wit.edu>
2012-11-12 17:55:56 -0500

    Added a class to track information to render the unit health HUD element.

---
+++ arya/src/arya/UnitHealthHUD.java
@@ -0,0 +1,38 @@
+package arya;
+
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.geom.Rectangle;
+
+/**
+ * Manages information used to render the unit health Heads Up Display object drawn over the game screen.
+ * @author youngb2
+ *
+ */
+public class UnitHealthHUD {
+	Graphics unitStats = new Graphics();
+	Image icon;
+	Rectangle background;
+	Rectangle healthTotal;
+	Rectangle healthCurrent;
+	String name = "Arya";
+	String hp = "HP:";
+	int health = 0;
+	int totalHealth = 100;
+	
+	/**
+	 * Constructor to create an appropriately scaled HUD for the game container
+	 * @param container		the AppGameContainer to build the health HUD for, provides information on size
+	 * @param unit			the unit who's health stats are to be rendered
+	 */
+	UnitHealthHUD(AppGameContainer container, BasicUnit unit) {
+		background.setSize((float)(container.getWidth()*.3), (float)(container.getHeight()*.2));
+		
+	}
+	
+	void render() {
+		
+	}
+}
